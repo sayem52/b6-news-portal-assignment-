b6539cfc30803bcfbf2014d9aa9abd6e043c0dc0
sayem52 <sayem15-12949@diu.edu.bd>
2022-09-06 02:25:55 +0600

    3rd

--- html/category.js
+++ html/category.js
@@ -15,30 +15,59 @@ data.forEach( category => {
    categoryNav.classList.add('navbar-nav');
    categoryNav.innerHTML=
    `
-   <a  onclick="categoryDetail()"  class="nav-link active" href="#">${category.category_name}</a>
+   <a onclick="apiDetail('${category.category_id}')"  class="nav-link active" href="#">${category.category_name}</a>
    `
-   
-
      navContainer.appendChild(categoryNav);
-     
-         
 
 })
-
 
    };
 
    loadUsers('categories');
    
 
-  categoryDetail=category_id=>{
-    const url =`  https://openapi.programming-hero.com/api/news/category/${category_id}`;
-    fetch(url)
+const apiDetail = id =>{
+   const url= `https://openapi.programming-hero.com/api/news/category/${id}`;
+   console.log(url);
+   fetch(url)
    .then(res => res.json())
-   .then(data => displayUser(data.data.news_category));
+   .then(data => displayDetail(data.data))
+   
+   };
 
-    console.log(url);
-  }
+const displayDetail = data =>{
+   
+       const detailContainer=document.getElementById('detail-container');
+       detailContainer.innerHTML=``;
+       data.forEach( item =>{
+         console.log(item);
+       const categoryDetail= document.createElement('div');
+       categoryDetail.classList.add('card');
+       categoryDetail.classList.add('card-m');
+
+       categoryDetail.innerHTML=
+       
+       `
+       <div class="row g-0">
+            <div class="col-md-4">
+              <img src=${item.image_url} class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">${item.title}</h5>
+                <p class="card-text">${item.details.slice(0,200)}</p>
+                
+              </div>
+            </div>
+        </div>
+       `
+       
+    
+       detailContainer.appendChild(categoryDetail);
+         
+      })
+ 
+       };
+
   
-
 
